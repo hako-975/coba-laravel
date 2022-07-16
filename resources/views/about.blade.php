a50f6ee0c84486c9f4b56cad94848a955ab0a922
hako-975 <andrifirmansaputra1@gmail.com>
2022-07-16 11:06:08 +0700

    part 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Andri Blog | About</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Halaman About</h1>
+	<h3><?= $name; ?></h3>
+	<p><?= $email; ?></p>
+	<img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Andri Firman Saputra',
+        'email' => 'andrifirmansaputra1@gmail.com',
+        'image' => 'andri.png' 
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
